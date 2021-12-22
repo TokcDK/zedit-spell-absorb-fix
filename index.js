b2fd2e4e8c579dee6b605ebac30a080662afb491
Denis K <tokcdk@gmail.com>
2021-12-22 20:28:27 +0500

    added trycatch to just skip record and continue patchng some error output stack in log

--- index.js
+++ index.js
@@ -11,39 +11,47 @@ registerPatcher({
             load: () => ({
                 signature: 'SPEL',
                 filter: function (record) {
-                    // check for the icon being the one used for summoning spells
-                    const icon = xelib.GetValue(record, "MDOB - Menu Display Object");
-                    if (!icon) {
-                        return false;
-                    }
-                    if (icon !== "MAGINVSummon [STAT:000A6459]") {
-                        return false;
-                    }
-                    // do not process records already having the flag
-                    if (xelib.GetFlag(record, 'SPIT - Data\\Flags', 'No Absorb/Reflect')) {
-                        return false;
-                    }
+                    try {
+                        // check for the icon being the one used for summoning spells
+                        const icon = xelib.GetValue(record, "MDOB - Menu Display Object");
+                        if (!icon) {
+                            return false;
+                        }
+                        if (icon !== "MAGINVSummon [STAT:000A6459]") {
+                            return false;
+                        }
+                        // do not process records already having the flag
+                        if (xelib.GetFlag(record, 'SPIT - Data\\Flags', 'No Absorb/Reflect')) {
+                            return false;
+                        }
+					} catch (e) {
+						return false;
+					}
                     return true;
                 }
             }),
             patch: function (record, helpers, settings, locals) {
-                helpers.logMessage(`Processing spell ${xelib.LongName(record)}`);
-                const effects = xelib.GetElements(record, "Effects", false);
-                const effectsLength = effects.length
-                for (let i = 0; i < effectsLength; i++) {
-                    const form_id = xelib.GetUIntValue(effects[i], "EFID - Base Effect");
-                    if (form_id === 0) {
-                        continue;
+                try {
+                    helpers.logMessage(`Processing spell ${xelib.LongName(record)}`);
+                    const effects = xelib.GetElements(record, "Effects", false);
+                    const effectsLength = effects.length
+                    for (let i = 0; i < effectsLength; i++) {
+                        const form_id = xelib.GetUIntValue(effects[i], "EFID - Base Effect");
+                        if (form_id === 0) {
+                            continue;
+                        }
+                        const eff_record = xelib.GetRecord(0, form_id);
+                        const archtype = xelib.GetValue(eff_record, "Magic Effect Data\\DATA - Data\\Archtype");
+                        xelib.Release(eff_record);
+                        if (archtype === 'Summon Creature') {
+                            helpers.logMessage(`Adding "absorb" to ${xelib.LongName(record)}`);
+                            xelib.SetFlag(record, 'SPIT - Data\\Flags', 'No Absorb/Reflect', true);
+                            return;
+                        }
                     }
-                    const eff_record = xelib.GetRecord(0, form_id);
-                    const archtype = xelib.GetValue(eff_record, "Magic Effect Data\\DATA - Data\\Archtype");
-                    xelib.Release(eff_record);
-                    if (archtype === 'Summon Creature') {
-                        helpers.logMessage(`Adding "absorb" to ${xelib.LongName(record)}`);
-                        xelib.SetFlag(record, 'SPIT - Data\\Flags', 'No Absorb/Reflect', true);
-                        return;
-                    }
-                }
+				} catch(e) {
+					helpers.logMessage(`Failed to patch spell ${xelib.LongName(record)}.\n>>>>>>\n${e.stack}\n<<<<<<\n\n`);
+				}
             }
         }],
     })
